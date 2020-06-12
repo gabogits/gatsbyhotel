34dc542703fa8ed2d69885a01ebe66090a8fb61c
Gabriel Martinez Miranda <gabriel@kubofinanciero.com>
2020-06-12 15:52:29 -0500

    primer commit

---
+++ src/pages/nosotros.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from '../components/layout';
+import ContenidoNosotros from '../components/ContenidoNosotros';
+
+const IndexPage = () => (
+  <Layout>
+
+    <ContenidoNosotros />
+  </Layout>
+
+)
+
+export default IndexPage
